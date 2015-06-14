0257998ac752751752b9b029078760d834e71250
m-ochi <masanao.oochi@gmail.com>
2015-06-14 17:10:32 +0900

    出力

--- analysis/makeNetwork.py
+++ analysis/makeNetwork.py
@@ -14,29 +14,97 @@ import json
 import pickle
 import re
 import csv
+import MeCab
 
-center_com = "わらべや日洋"
+center_coms = ["わらべや日洋","セブンイレブン"]
+#center_coms = ["わらべや日洋"]
+outfile = "data.json"
+
 
 def run():
-    companyfile = "c_names.pickle"
-    tweetcsv = "allTweetWarabeya.csv"
+    companyfile = "c_names.csv"
+    tweetcsvs = ["allTweetWarabeya.csv","allTweetSeven.csv"]
+#    tweetcsvs = ["allTweetWarabeya.csv"]
     companies = getCompanies(companyfile)
-    center_idx = companies.index(center_com)
-    print "center_idx:%d"%(center_idx)
-    comratedic = makeRateList(companies, tweetcsv, center_idx)
-    print comratedic
 
     outDic = {}
-    outDic["company"] = center_com
-    outDic["trading"] = comratedic
+    outDic["data"] = []
+    for i,tweetcsv in enumerate(tweetcsvs):
+#        if i+1 % 10 == 0:
+#            print "count: %d"%(i+1)
+        print "count: %d"%(i+1)
+        center_com = center_coms[i]
+        print center_com
+        center_idx = companies.index(center_com)
+        print "center_idx:%d"%(center_idx)
+        comratedic,alltweets = makeRateList(companies, tweetcsv, center_idx)
+#        print comratedic
+
+        aDic = {}
+        aDic["company"] = center_com
+        aDic["trading"] = comratedic
+        keywords = getKeywords(alltweets)
+#        print "keywords"
+#        print keywords
+        aDic["keywords"] = keywords
+        outDic["data"].append(aDic)
+
+    #outfile
+#    print outDic
+    f = open(outfile, 'w')
+    json.dump(outDic,f,sort_keys=True,indent=4)
+    f.close()
+
+def getKeywords(alltweets):
+    tagger = MeCab.Tagger("-Ochasen")
+    ptn = re.compile("名詞-固有名詞")
+    eptn = re.compile(r'^[0-9A-Za-z]+')
+    namedentities = []
+    for tweet in alltweets:
+#        print tweet
+        a = tagger.parse(tweet)
+        ws = a.split('\t')
+ 
+        for i,w in enumerate(ws):
+            if ptn.search(w):
+                new_w = ws[i-3].strip()
+                if not eptn.match(new_w):
+                    namedentities.append(new_w)
+   
+    keywords = countNEs(namedentities)
+
+    return keywords
+
+def countNEs(namedentities):
+    vocab = sorted(list(set(namedentities)))
+    vocabdic = {}
+    for v in vocab:
+        vocabdic[v] = 0
+    for w in namedentities:
+        vocabdic[w] += 1
+
+    keywords = []
+    for v in vocab:
+        name = v
+        size = vocabdic[v]
+        adic = {}
+        adic["name"] = name
+        adic["size"] = size
+        keywords.append(adic)
+    return keywords
 
 def makeRateList(companies, tweetcsv, center_idx):
     countlist = [0]*len(companies)
     f = open(tweetcsv, 'r')
     reader = csv.reader(f)
+    alltweets = []
     for row in reader:
         tweet = row[4]
+        alltweets.append(tweet)
         for i, com in enumerate(companies):
+            if i % 10 == 0:
+                print i
+ 
             if i == center_idx:
                 continue
             ptn = re.compile(com)
@@ -54,13 +122,19 @@ def makeRateList(companies, tweetcsv, center_idx):
     for i,com in enumerate(companies):
         a_rate = rates[i]
         if a_rate > 0:
+            print com 
+            print a_rate
             comratedic[com] = a_rate
 
-    return comratedic
+    return comratedic, alltweets
 
 def getCompanies(companyfile):
     f = open(companyfile, 'r')
-    companies = pickle.load(f)
+    reader = csv.reader(f)
+    companies = []
+    for row in reader:
+        companies.append(row[0])
+#    companies = pickle.load(f)
     f.close()
     return companies
 
