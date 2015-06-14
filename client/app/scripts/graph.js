e5ec96d324dbd5003be7a738282bb8cc56b0ee4e
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-06-14 16:36:23 +0900

    initial commit for keyword graph

--- client/app/scripts/graph.js
+++ client/app/scripts/graph.js
@@ -15,7 +15,7 @@ var tradingGraph = function(){
 	var diagonal = d3.svg.diagonal()
 		.projection(function(d) { return [d.y, d.x]; });
 
-	var svg = d3.select("body").append("svg")
+	var svg = d3.select(".trending-graph").append("svg")
 		.attr("width", width + margin.left + margin.right)
 		.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -143,6 +143,63 @@ var tradingGraph = function(){
 
 
 var keywordGraph = function() {
-	
+	var diameter = 960,
+		format = d3.format(",d"),
+		color = d3.scale.category20c();
+
+	var bubble = d3.layout.pack()
+		.sort(null)
+		.size([diameter, diameter])
+		.padding(1.5);
+
+	var svg = d3.select(".keywords-graph").append("svg")
+		.attr("width", diameter)
+		.attr("height", diameter)
+		.attr("class", "bubble");
+
+	d3.json("data.json", function(error, root) {
+		if (error) throw error;
+
+
+		var data = root.data[0];
+		var rootNode = {
+			"name": data.company,
+			"children": data.keywords
+		};
+
+		var node = svg.selectAll(".node")
+			.data(bubble.nodes(classes(rootNode))
+				.filter(function(d) { return !d.children; }))
+			.enter().append("g")
+			.attr("class", "node")
+			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		node.append("title")
+			.text(function(d) { return d.className + ": " + format(d.value); });
+
+		node.append("circle")
+			.attr("r", function(d) { return d.r; })
+			.style("fill", function(d) { return color(d.packageName); });
+
+		node.append("text")
+			.attr("dy", ".3em")
+			.style("text-anchor", "middle")
+			.text(function(d) { return d.className.substring(0, d.r / 3); });
+	});
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+	function classes(root) {
+		var classes = [];
+
+		function recurse(name, node) {
+			if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+			else classes.push({packageName: name, className: node.name, value: node.size});
+		}
+
+		recurse(null, root);
+		return {children: classes};
+	}
+
+	d3.select(self.frameElement).style("height", diameter + "px");
 }();
 
