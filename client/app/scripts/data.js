22c6a09567c645406b8281b402e1775858ff9865
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-06-14 14:19:08 +0900

    adding sample data js

---
+++ client/app/scripts/data.js
@@ -0,0 +1,23 @@
+var data = {
+	"company": "わらべや日洋",
+	"trading": [
+		{
+			"name": "セブンイレブン",
+			"weight": 0.7
+		},
+		{
+			"name": "フジフーズ",
+			"weight": 0.3
+		}
+	],
+	"keywords": [
+		{
+			"name": "おにぎり",
+			"weight": 300
+		},
+		{
+			"name": "サンドイッチ",
+			"weight": 200
+		}
+	]
+}
