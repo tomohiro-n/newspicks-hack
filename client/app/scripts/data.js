ea5eadb72830c8a4d413d8a266883b2f9298ebd7
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-06-14 14:22:43 +0900

    made the data array so we can easily add other companies later

--- client/app/scripts/data.js
+++ client/app/scripts/data.js
@@ -1,23 +1,25 @@
-var data = {
-	"company": "わらべや日洋",
-	"trading": [
-		{
-			"name": "セブンイレブン",
-			"weight": 0.7
-		},
-		{
-			"name": "フジフーズ",
-			"weight": 0.3
-		}
-	],
-	"keywords": [
-		{
-			"name": "おにぎり",
-			"weight": 300
-		},
-		{
-			"name": "サンドイッチ",
-			"weight": 200
-		}
-	]
-}
+var data = [
+	{
+		"company": "わらべや日洋",
+		"trading": [
+			{
+				"name": "セブンイレブン",
+				"weight": 0.7
+			},
+			{
+				"name": "フジフーズ",
+				"weight": 0.3
+			}
+		],
+		"keywords": [
+			{
+				"name": "おにぎり",
+				"weight": 300
+			},
+			{
+				"name": "サンドイッチ",
+				"weight": 200
+			}
+		]
+	}
+];
